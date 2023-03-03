5951fb2c207302ab0622711edec420ba6471ed3f
Kailen James <sevenflags57@gmail.com>
2023-03-03 11:07:22 -0800

    Created footer

--- src/components/main-footer/index.js
+++ src/components/main-footer/index.js
@@ -4,7 +4,9 @@ import "./style.css";
 export default function MainFooter() {
 	return (
 		<footer className="main-footer">
-			
+			<a href="https://github.com/SprocketCreations" target="_blank">GitHub</a>
+			<a href="https://www.linkedin.com/in/kailen-james-33471717b/" target="_blank">LinkdIn</a>
+			<a href="http://parallaxarchive.com/portfolio">My Website</a>
 		</footer>
 	);
 }
