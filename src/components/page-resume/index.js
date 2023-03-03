4b69b1c8752278fa6cfb73ce78417cb0e675cf4a
Kailen James <sevenflags57@gmail.com>
2023-03-03 11:07:43 -0800

    Built resume page

--- src/components/page-resume/index.js
+++ src/components/page-resume/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./style.css";
 
+/** @type {string[]} The skills to list on the page. */
+const proficiencies = [
+	"Javascript",
+	"React",
+	"Git",
+	"mySQL",
+	"C++"
+];
+
 export default function PageResume() {
 	return (
 		<main className="page-resume">
-			Resume
+			<article>
+				<h2>Resume</h2>
+				<a href="http://">Download</a>
+			</article>
+			<article>
+				<h3>Skills</h3>
+				<ol>
+					{proficiencies.map(skill => <li>{skill}</li>)}
+				</ol>
+			</article>
 		</main>
 	);
 }
