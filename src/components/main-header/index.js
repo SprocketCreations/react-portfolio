4b868644e15f4e42262eff81dae83a514d08d5ed
Kailen James <sevenflags57@gmail.com>
2023-03-05 17:32:01 -0800

    Styled header

--- src/components/main-header/index.js
+++ src/components/main-header/index.js
@@ -1,6 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./style.css";
+
+const routes = [
+	{
+		route: "/",
+		render: "About Me"
+	}, {
+		route: "/portfolio",
+		render: "Portfolio"
+	}, {
+		route: "/contact",
+		render: "Contact"
+	}, {
+		route: "/resume",
+		render: "Resume"
+	}
+]
 
 /**
  * @typedef MainHeaderProps The props for a MainHeader element.
@@ -11,17 +27,16 @@ import "./style.css";
  * @returns {JSX.Element}
  */
 export default function MainHeader() {
+	const {pathname: location} = useLocation();
+
 	return (
 		<header className="main-header">
 			<nav>
 				<h1>Kailen James</h1>
 				<ul>
-					<Link to="/">About Me</Link>
-					<Link to="/portfolio">Portfolio</Link>
-					<Link to="/contact">Contact</Link>
-					<Link to="/resume">Resume</Link>
-				</ul>
-			</nav>
-		</header>
+					{routes.map(({route, render}, i) => <li key={i}><Link className={location == route? "active-tab": ""} to={route}>{render}</Link></li>)}
+				</ul >
+			</nav >
+		</header >
 	);
 }
