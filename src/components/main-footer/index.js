b6bd6aa712d59228cc4cc43a82dc4b463858168f
Kailen James <sevenflags57@gmail.com>
2023-03-05 17:31:36 -0800

    Styled footer

--- src/components/main-footer/index.js
+++ src/components/main-footer/index.js
@@ -4,9 +4,11 @@ import "./style.css";
 export default function MainFooter() {
 	return (
 		<footer className="main-footer">
-			<a href="https://github.com/SprocketCreations" target="_blank">GitHub</a>
-			<a href="https://www.linkedin.com/in/kailen-james-33471717b/" target="_blank">LinkdIn</a>
-			<a href="http://parallaxarchive.com/portfolio">My Website</a>
+			<ul>
+				<li><a href="https://github.com/SprocketCreations" target="_blank">GitHub</a></li>
+				<li><a href="https://www.linkedin.com/in/kailen-james-33471717b/" target="_blank">LinkdIn</a></li>
+				<li><a href="http://parallaxarchive.com/portfolio">My Website</a></li>
+			</ul>
 		</footer>
 	);
 }
